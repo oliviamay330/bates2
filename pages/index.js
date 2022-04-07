ee7392f90c79850211b716ebb60b605c5663b71c
Emily Healy <emilyhealy98@gmail.com>
2022-04-07 15:04:50 -0400

    use jwt instead of previous auth

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,20 @@ import styles from "../styles/Home.module.css";
 import { google } from "googleapis";
 
 export async function getServerSideProps({ query }) {
-  const auth = await google.auth.getClient({
-    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-  });
-
-  const sheets = google.sheets({ version: "v4", auth });
-
+  const target = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+  const jwt = new google.auth.JWT(
+    process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    null,
+    (process.env.GOOGLE_SHEETS_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
+    target
+  );
+  const sheets = google.sheets({ version: "v4", auth: jwt });
   const response = await sheets.spreadsheets.values.get({
-    spreadsheetId: process.env.SHEET_ID,
+    spreadsheetId: process.env.SPREADSHEET_ID,
     range: "Sheet1!A2:B",
   });
 
   const content = response.data.values.reverse();
-  console.log("stuff", content);
 
   return {
     props: {
